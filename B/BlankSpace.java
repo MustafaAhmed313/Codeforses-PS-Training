0d29a7e5ebf28b024bae6af6f3cbc2db07568a0c
MustafaAhmed313 <mostafa3132004@gmail.com>
2023-05-06 22:28:00 +0300

    div4 b cf

--- B/BlankSpace.java
+++ B/BlankSpace.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class BlankSpace {
+import java.util.Scanner;
+public class BlankSpace {
+    static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        int t = input.nextInt();
+        for (int i = 0 ; i < t ; i++) { //stack[100010001]
+            int n = input.nextInt();
+            int[]a = new int[n];
+            int max = 0 , count = 0;
+            for (int j = 0 ; j < n ; j++) { //0
+                a[j] = input.nextInt();
+                if (a[j] == 0) count++; //j = 8 | count = 3
+                else  {
+                    max = Math.max(max , count);
+                    count = 0;
+                }
+            }
+            System.out.println(Math.max(max , count));
+        }
+    }
 }
--- B/ChatOrder.cpp
+++ B/ChatOrder.cpp
@@ -2,15 +2,36 @@
 // Created by Mostafa on 5/4/2023.
 //
 #include <iostream>
-#include <string>
 #include <vector>
 #include <map>
 using namespace std;
+//bool canPartitionKSubsets(vector<int>& nums, int k) {
+//    map<int , int> m;
+//    for (int i = 0 ; i < (1 << nums.size()) ; i++) { //4,3,2,3,5,2,1
+//
+//        int sum = 0;
+//        for (int j = 0 ; j < nums.size() ; j++)
+//            if ((1 << j) & i) sum += nums[j];
+//        m[sum]++;
+//    }
+//    for (auto e : m)
+//        if (e.second % k == 0) return true;
+//    return false;
+//}
 int main() {
-    int n;cin >> n;
-    map<string , int> m;
-    for (int i = 0 ; i < n ; i++) {
-        string person; cin >> person;
-        m[person]++;
+    int t;cin >> t;
+
+    for (int i = 0 ; i < t ; i++) {
+        map<string , int>m;
+        m["00"] = m["10"] = m["01"] = m["11"] = 1e9;
+        int n;cin >> n;
+        for (int j = 0 ; j < n ; j++) {
+            int minute;cin >>minute;
+            string skill;cin >> skill;
+            m[skill] = min(minute , m[skill]);
+        }
+        if (min(m["11"] , m["10"] + m["01"]) > (int) 1e6) cout << "-1" << endl;
+        else cout << min(m["11"] , m["10"] + m["01"]) << endl;
     }
+    return 0;
 }
