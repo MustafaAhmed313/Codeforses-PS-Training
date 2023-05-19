479e75a3966787a358f8f0bf52d6eaa6eedfa4cb
MustafaAhmed313 <mostafa3132004@gmail.com>
2023-05-19 16:09:14 +0300

    done

--- A/Candies.java
+++ A/Candies.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Candies {
+import java.util.*;
+public class Candies {
+    static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        int t = input.nextInt();
+        for (int i = 0 ; i < t ; i++) {
+            int n = input.nextInt();
+            int k = 2;
+            while (true) {
+                if(n % (((int)Math.pow(2 , k)) - 1) == 0) {
+                    System.out.println(n / (((int)Math.pow(2 , k)) - 1));
+                    break;
+                }
+                k++;
+            }
+        }
+    }
 }
--- B/BalancedArray.java
+++ B/BalancedArray.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class BalancedArray {
+import java.util.*;
+public class BalancedArray {
+    static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        int t = input.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = input.nextInt();
+            if (n % 4 != 0) {
+                System.out.println("NO");
+                continue;
+            }
+            System.out.println("YES");
+            for (int j = 2; j <= n; j += 2) {
+                System.out.print(j + " ");
+            }
+            int j;
+            for (j = 1; j < n - 1; j += 2) {
+                System.out.print(j + " ");
+            }
+            System.out.println(j + n / 2);
+        }
+    }
 }
+
